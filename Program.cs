b9c729b1c19555026d78d03c32c1cd3c4f054ae7
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-10-03 13:45:49 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+namespace DotnetServicePython
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("JobExecutionContext", "AgendadorDeTarefas")
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}][{Level:u3}][{JobExecutionContext}] {Message:lj}{NewLine}")
+                .CreateLogger();
+
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<Service>();
+                })
+                .UseSerilog(Log.Logger);
+        }
+    }
+}
